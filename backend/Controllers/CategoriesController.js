4fc1a011cfede2b846a1aa30b5a5d40c4da6c221
kareemSh30 <keyofawesome55@gmail.com>
2025-02-06 02:03:50 +0200

    add update and delete routes

--- backend/Controllers/CategoriesController.js
+++ backend/Controllers/CategoriesController.js
@@ -13,3 +13,47 @@ export const CreateCategory = async (req, res) => {
         res.status(500).json({ error: "Server error" });
     }
 };
+
+export const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.status(200).json(categories);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+export const getcategoryById = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+export const updateCategory = async (req, res) => {
+    try {
+        const category = await Category.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+export const deleteCategory = async (req, res) => {
+    try {
+        const category = await Category.findByIdAndDelete(req.params.id);
+        res.status(200).json(category);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+};
--- backend/Routes/CategoriesRoute.js
+++ backend/Routes/CategoriesRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { CreateCategory } from "../Controllers/CategoriesController.js";
+import { CreateCategory, getAllCategories, getcategoryById, deleteCategory, updateCategory } from "../Controllers/CategoriesController.js";
+import { protectmiddleware } from "../Utils/authmiddleware.js";
 
 const categoriesRouter = express.Router();
 
 
 categoriesRouter.post("/createcategory", CreateCategory);
+categoriesRouter.get("/getallcategories", getAllCategories);
+categoriesRouter.get("/getcategorybyid/:id", getcategoryById);
+categoriesRouter.delete("/deletecategory/:id",protectmiddleware, deleteCategory);
+categoriesRouter.put("/updatecategory/:id",protectmiddleware, updateCategory);
 
 export default categoriesRouter;
